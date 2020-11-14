9d5e757d0d5730deeb0a569393db048457be1c94
Chong Zhang <zchoniong@gmail.com>
2020-11-13 21:17:48 -0800

    Train with 3 layers

--- examples/language/train.sh
+++ examples/language/train.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-python train.py --dir=model_data/transformer_cert --robust --method=IBP+backward_train --train
-python train.py --dir=model_data/transformer_normal --train
+args='--max_sent_length 128 --num_layers 3'
+
+python train.py --dir=model_data/transformer_cert --robust --method=IBP+backward_train --train $args
+python train.py --dir=model_data/transformer_normal --train $args
